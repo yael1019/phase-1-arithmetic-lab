654ed840853a3b214c3ee1827e13871d53d505aa
Yael Harosh <yaelharosh01@gmail.com>
2023-02-20 13:51:03 -0500

    pass all math tests

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22;
+let num4 = 18;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 5, 6, 10, 20);
